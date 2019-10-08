8333d0e0378a910ccc6a040770c70dda18435e3a
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2019-10-08 01:45:52 -0300

    first commit

---
+++ 1-ByteBank/ContaCorrente.cs
@@ -0,0 +1,8 @@
+﻿//MINHA PRIMEIRA CLASSE DE CONTA CORRENTE
+public class ContaCorrente
+{
+       public string titular;
+       public int agencia;
+       public int numero;
+       public double saldo;
+}
---
+++ 1-ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new ContaCorrente(); //estamos criando na memoria do  computador / minha primeira classe / primeiro objeto
+
+            ContaCorrente contaDaBruna = new ContaCorrente();
+            contaDaBruna.titular = "Bruna"; // dentro da linha titular tem o nome da Bruna
+            contaDaBruna.agencia = 863;
+            contaDaBruna.numero = 863452;
+            contaDaBruna.saldo = 100;
+
+            Console.WriteLine(contaDaBruna.titular);
+            Console.WriteLine("Agência: " +  contaDaBruna.agencia);
+            Console.WriteLine("Número: " +  contaDaBruna.numero);
+            Console.WriteLine("Saldp: " +  contaDaBruna.saldo);
+
+            contaDaBruna.saldo += 200;
+
+            Console.WriteLine("Novo Saldo: " + contaDaBruna.saldo);
+
+            
+            Console.ReadLine();
+           
+        }
+    }
+}
