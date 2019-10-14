a3ef12e4aa02073362cfd73b5e2315ad32d13a50
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2019-10-14 00:23:39 -0300

    finalizando a aula 4

---
+++ 5-ByteBank/ContaCorrente.cs
@@ -0,0 +1,49 @@
+﻿//MINHA PRIMEIRA CLASSE DE CONTA CORRENTE
+
+//using _5_ByteBank; com o namespace essa linha torna-se desnecess
+
+namespace _5_ByteBank
+{
+    public class ContaCorrente
+    {
+        //public _5_ByteBank.Cliente titular;
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        public bool Sacar(double valor) // usases verbos em caso de criação de função; temos que colocar // PODEMOS CHAMAR DE FUNÇÃO PORQUE ELE RETORNA ALGO
+        {
+            if (this.saldo < valor) //this é uma paçavra reservada que diz que eu estou acessando o saldo desssa isntancia, desse objeto
+            {
+                return false;
+            }
+            else // somso obrigados a colocar o else porque garantimos que essa função retorna um bool
+            {
+                this.saldo -= valor;
+
+                return true;
+            }
+        }
+        public void Depositar(double valor) // como nao tem retorno e portanto colocamos VOID // PODEMOS CHAMAR DE METODOS PORQUE TEM O VOID
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+    }
+}
+
+    
---
+++ 5-ByteBank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente bruna = new Cliente();
+
+           // bruna.nome = "Bruna Costa";
+           // bruna.profissao = "Desenvolvedora C#";
+           // bruna.cpf= "434.562.878-20";
+           
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = bruna;
+            //conta.titular = new Cliente();
+
+            //conta.titular.nome = "Bruna Neri";
+            //conta.titular.cpf = "434.562.878-20";
+            //conta.titular.profissao = "Desenvolvedora C#";
+
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            if(conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referêcia em conta, titular é NULL");
+            }
+            //Console.WriteLine(bruna.nome); // é o mesmo que o objeto a baixo . São refenrcia ´para o mesmo objeto na memoria do computador
+            Console.WriteLine(conta.titular); // é uma referencia vazia, ou seja nao retorna nada  - >> null
+                        
+            
+            //Console.WriteLine(conta.titular.nome); // é analogo a linha a cima, mas de forma diferente
+            //Console.WriteLine(conta.titular.cpf);
+            //Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+
+ 
+            
+        }
+    }
+}
