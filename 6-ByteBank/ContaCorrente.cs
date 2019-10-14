9a2f09785c811930f51b140f00d5e0a4a3302686
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2019-10-14 01:04:21 -0300

    finalizando a aula 5

---
+++ 6-ByteBank/ContaCorrente.cs
@@ -0,0 +1,105 @@
+﻿//MINHA PRIMEIRA CLASSE DE CONTA CORRENTE
+
+//using _5_ByteBank; com o namespace essa linha torna-se desnecess
+
+namespace _6_ByteBank
+{
+    public class ContaCorrente
+    {
+        //public _5_ByteBank.Cliente titular;
+        //public Cliente titular;
+        private Cliente _titular;
+        public int agencia;
+        public int numero;
+        private double _saldo = 100; //só é visivel dentro da classe
+
+
+        public Cliente Titular
+        {
+
+            get;
+            set;
+
+            //simplificando e nao muda nada // USA-SE QUANDO NAO TEM NENHUMA REGRA
+
+           // get
+           // {
+           //     return _titular;
+           // }
+           // set
+           // {
+           //     _titular = value;
+           // }
+        }
+        public double Saldo // propriedade
+        {
+            get // sempre retorna
+            {
+                return _saldo; // precisa ter um retorno // _saldo é um campo privado
+            }
+
+            set // define o valor saldo
+            {
+                if (value < 0) // o valor que estamos recebendo que seria saldo
+                {
+                    return; // para de executar quando para assim
+                            //verifica o argumento saldo
+                }
+
+                this._saldo = value; // acessa o campo saldo // acontece colisao de nomes entao temos que colcocar o this
+            }
+        }
+
+       //public void SetSaldo(double saldo) //definir == set
+       //{
+       //    if(saldo < 0)
+       //    {
+       //        return; // para de executar quando para assim
+       //        //verifica o argumento saldo
+       //    }
+       //    
+       //    this.saldo = saldo; // acessa o campo saldo // acontece colisao de nomes entao temos que colcocar o this
+       //    
+       //}
+
+       //public double GetSaldo() //  obter == get
+       //{
+       //    return saldo;
+       //}
+
+        public bool Sacar(double valor) // usases verbos em caso de criação de função; temos que colocar // PODEMOS CHAMAR DE FUNÇÃO PORQUE ELE RETORNA ALGO
+        {
+            if (_saldo < valor) //this é uma paçavra reservada que diz que eu estou acessando o saldo desssa isntancia, desse objeto
+            {
+                return false;
+            }
+            else // somso obrigados a colocar o else porque garantimos que essa função retorna um bool
+            {
+                _saldo -= valor;
+
+                return true;
+            }
+        }
+        public void Depositar(double valor) // como nao tem retorno e portanto colocamos VOID // PODEMOS CHAMAR DE METODOS PORQUE TEM O VOID
+        {
+            //this.saldo += valor; não é necessario colocar o this porque nao tem conflito de argumentos ja que tem o nome diferentes
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+    }
+}
+
+    
---
+++ 6-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            Cliente cliente = new Cliente();
+            cliente.nome = "Guilherme";
+            cliente.cpf = "434.564.879-20";
+            cliente.profissao = "Desenvolvedor c#";
+
+
+            conta.Saldo = -10;
+            conta.Titular = cliente; // usa-se a propri titular
+
+            //Console.WriteLine(conta.GetSaldo());
+            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Saldo);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
